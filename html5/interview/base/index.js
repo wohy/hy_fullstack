8880e55d2678fa98c277e4a4112f79de2ca23f2b
wohy <1829757133@qq.com>
2020-11-18 23:56:03 +0800

    js更新

---
+++ html5/interview/base/index.js
@@ -0,0 +1,9 @@
+let key1 = 1
+let key2 = 1
+
+// console.log(key1 === key2);
+
+// console.log('hello'.length) 原始值是没有属性没有方法的， 这里的length是String对象上的属性 字符串创建后会继承String上的方法
+
+
+// 0.1 + 0.2 == 0.3   错误 精度丢失
---
+++ html5/interview/base/readme.md
@@ -0,0 +1,5 @@
+# 原始类型有几种？
+- boolean null unidefined number string symbol
+
+# null 是对象吗？
+- 是的 
