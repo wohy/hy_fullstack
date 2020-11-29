79e873b9c190f0702b0b333d333e9abbb8048fe8
wohy <1829757133@qq.com>
2020-11-29 23:10:38 +0800

    vue初始

--- vue/vue-weather/src/views/index.vue
+++ vue/vue-weather/src/views/index.vue
@@ -7,10 +7,10 @@
 
     <div class="city-info">
       <p class="city">{{locationInfo}}</p>
-      <p class="weather">阴</p>
-      <h2 class="temp"><em>9</em>℃</h2>
+      <p class="weather">{{weather}}</p>
+      <h2 class="temp"><em>{{temperature}}</em>℃</h2>
       <div class="detali">
-        <span>风力:3</span> | <span>风向:西北</span> | <span>空气湿度: 75%</span>
+        <span>风力:{{windPower}}</span> | <span>风向:{{windDir}}</span> | <span>空气湿度: {{humidity}}</span>
       </div>
     </div>
   </div>
@@ -21,20 +21,29 @@ export default {
   data () {
     return {
       localTime: '',
-      locationInfo: ''
+      locationInfo: '',
+      windPower:'',
+      windDir:'',
+      humidity:'',
+      temperature: '',
+      weather: '',
     }
   },
   created () {
     setInterval(() => {
       this.localTime = this.getLocalTime()
     }, 1000)
-     console.log(this.initMap()); 
+    this.initMap() 
+  },
+  updated () {
+    this.getCurrentCityData()
   },
   methods: {
     getLocalTime () {
       return new Date().toLocaleTimeString()
     },
     initMap () { //获取当前城市
+      let that = this
       AMap.plugin('AMap.Geolocation', function() {
         var geolocation = new AMap.Geolocation({
           // 是否使用高精度定位，默认：true
@@ -49,21 +58,53 @@ export default {
           buttonPosition: 'RB'
         })
 
-        return geolocation.getCurrentPosition()
-      //   AMap.event.addListener(geolocation, 'complete', onComplete)
-      //   AMap.event.addListener(geolocation, 'error', onError)
+        geolocation.getCurrentPosition()
+        AMap.event.addListener(geolocation, 'complete', onComplete)
+        AMap.event.addListener(geolocation, 'error', onError)
 
-      //   function onComplete (data) {
-      //     // data是具体的定位信息
-      //   }
-
-      //   function onError (data) {
-      //     // 定位出错
-      //   }
-      })  
+        function onComplete (data) {
+            // data是具体的定位信息
+            // console.log(data.formattedAddress);
+            // console.log('定位完毕');
+            // console.log(data.addressComponent);
+            that.locationInfo = data.addressComponent.city
+          }
+        function onError (data) {
+          // 定位出错
+        }
+      
+    })
+        
     },
     getCurrentCityData () { //查询天气
+      let that = this
 
+      AMap.plugin('AMap.Weather', function() {
+        //创建天气查询实例
+        var weather = new AMap.Weather();
+        let local = that.locationInfo
+        //执行实时天气信息查询
+        weather.getLive(local, function(err, data) {
+          // console.log(err, data);
+          that.temperature = data.temperature;
+          that.windPower = data.windPower;
+          that.windDir = data.windDirection;
+          that.humidity = data.humidity;
+        });
+      });
+
+      // AMap.plugin('AMap.Weather', function() {
+      //   //创建天气查询实例
+      //   let weather = new AMap.Weather();
+      //   let local = that.locationInfo
+      //   // console.log(local);
+
+      //   //执行实时天气信息查询
+      //   weather.getForecast(local, function(err, data) {
+      //     // console.log(err, data);
+      //     // console.log(data.forecasts[0]);
+      //   });
+      // });
     }
   }
 }
@@ -82,8 +123,8 @@ export default {
   }
   .city-info{
     font-size: 26px;
-    .em{
-      font-size: 34px;
+    em{
+      font-size: 40px;
       font-style: normal;
     }
   }
