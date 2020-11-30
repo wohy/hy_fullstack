70273087ecfa5c17ced6bcb2660229daaa57c1aa
wohy <1829757133@qq.com>
2020-11-30 17:57:51 +0800

    vue天气

---
+++ vue/vue-elm/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app">
+    <v-header></v-header>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/Header'
+export default {
+  components: {
+    'v-header': header
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- vue/vue-weather/src/views/index.vue
+++ vue/vue-weather/src/views/index.vue
@@ -1,132 +1,212 @@
 <template>
   <div class="container">
     <div class="nav">
-      <div class="time">{{localTime}}</div>
+      <div class="time">{{ localTime }}</div>
       <div class="city">切换城市</div>
     </div>
 
     <div class="city-info">
-      <p class="city">{{locationInfo}}</p>
-      <p class="weather">{{weather}}</p>
-      <h2 class="temp"><em>{{temperature}}</em>℃</h2>
-      <div class="detali">
-        <span>风力:{{windPower}}</span> | <span>风向:{{windDir}}</span> | <span>空气湿度: {{humidity}}</span>
+      <p class="city">{{ mapData.city }}</p>
+      <p class="weather">{{ mapData.weather }}</p>
+      <h2 class="temp">
+        <em>{{ mapData.temperature }}</em
+        >℃
+      </h2>
+      <div class="detail">
+        <span>风力:{{ mapData.windPower }}</span> |
+        <span>风向:{{ mapData.windDirection }}</span> |
+        <span>空气湿度: {{ mapData.humidity }}%</span>
       </div>
     </div>
+
+    <div class="future" v-if="futureMapData.length > 0">
+      <!-- v-if指令 若数组的长度大于0时才 future的dom结构才进行展示 -->
+      <div class="group">
+        明天:
+        <span class="tm"
+          >白天: {{ futureMapData[0].dayTemp }}
+          {{ futureMapData[0].dayWeather }} {{ futureMapData[0].dayWindDir }}
+          {{ futureMapData[0].dayWindPower }}</span
+        >
+        <span class="tm"
+          >夜间: {{ futureMapData[0].nightTemp }}
+          {{ futureMapData[0].nightWeather }}
+          {{ futureMapData[0].nightWindDir }}
+          {{ futureMapData[0].nightWindPower }}</span
+        >
+      </div>
+      <div class="group">
+        后天:
+        <span class="tm"
+          >白天: {{ futureMapData[1].dayTemp }}
+          {{ futureMapData[1].dayWeather }} {{ futureMapData[1].dayWindDir }}
+          {{ futureMapData[1].dayWindPower }}</span
+        >
+        <span class="tm"
+          >夜间: {{ futureMapData[1].nightTemp }}
+          {{ futureMapData[1].nightWeather }}
+          {{ futureMapData[1].nightWindDir }}
+          {{ futureMapData[1].nightWindPower }}</span
+        >
+      </div>
+    </div>
+
+    <div class="map-container" ref="mapContainer"></div>
+    <div class="echart-container" ref="echartContainer"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      localTime: '',
-      locationInfo: '',
-      windPower:'',
-      windDir:'',
-      humidity:'',
-      temperature: '',
-      weather: '',
-    }
+      localTime: "",
+      mapData: {},
+      futureMapData: [],
+      seriesData: []
+    };
   },
-  created () {
+  created() {
     setInterval(() => {
-      this.localTime = this.getLocalTime()
-    }, 1000)
-    this.initMap() 
+      this.localTime = this.getLocalTime();
+    }, 1000);
   },
-  updated () {
-    this.getCurrentCityData()
+  mounted() {
+    this.initMap();
   },
   methods: {
-    getLocalTime () {
-      return new Date().toLocaleTimeString()
+    getLocalTime() {
+      return new Date().toLocaleTimeString();
     },
-    initMap () { //获取当前城市
-      let that = this
-      AMap.plugin('AMap.Geolocation', function() {
-        var geolocation = new AMap.Geolocation({
-          // 是否使用高精度定位，默认：true
-          enableHighAccuracy: true,
-          // 设置定位超时时间，默认：无穷大
-          timeout: 10000,
-          // 定位按钮的停靠位置的偏移量，默认：Pixel(10, 20)
-          buttonOffset: new AMap.Pixel(10, 20),
-          //  定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-          zoomToAccuracy: true,     
-          //  定位按钮的排放位置,  RB表示右下
-          buttonPosition: 'RB'
-        })
-
-        geolocation.getCurrentPosition()
-        AMap.event.addListener(geolocation, 'complete', onComplete)
-        AMap.event.addListener(geolocation, 'error', onError)
-
-        function onComplete (data) {
-            // data是具体的定位信息
-            // console.log(data.formattedAddress);
-            // console.log('定位完毕');
-            // console.log(data.addressComponent);
-            that.locationInfo = data.addressComponent.city
+    initMap() {
+      // 获取当前城市
+      let _this = this;
+      var map = new AMap.Map(_this.$refs.mapContainer, {
+        resizeEnable: true,
+      });
+      AMap.plugin("AMap.CitySearch", function () {
+        var citySearch = new AMap.CitySearch();
+        citySearch.getLocalCity(function (status, result) {
+          if (status === "complete" && result.info === "OK") {
+            // 查询成功，result即为当前所在城市信息
+            // console.log(result);
+            _this.getCurrentCityData(result.city);  //将输出结果的city作为实参传给getCurrentCityData的cityname
           }
-        function onError (data) {
-          // 定位出错
-        }
-      
-    })
-        
-    },
-    getCurrentCityData () { //查询天气
-      let that = this
-
-      AMap.plugin('AMap.Weather', function() {
-        //创建天气查询实例
-        var weather = new AMap.Weather();
-        let local = that.locationInfo
-        //执行实时天气信息查询
-        weather.getLive(local, function(err, data) {
-          // console.log(err, data);
-          that.temperature = data.temperature;
-          that.windPower = data.windPower;
-          that.windDir = data.windDirection;
-          that.humidity = data.humidity;
         });
       });
+    },
+    getCurrentCityData(cityName) {
+      let _this = this;
+      // 查询天气
+      //加载天气查询插件
+      AMap.plugin("AMap.Weather", function () {
+        //创建天气查询实例
+        var weather = new AMap.Weather();
+        //执行实时天气信息查询
+        weather.getLive(cityName, function (err, data) {
+          // console.log(err, data);
+          _this.mapData = data; // 将返回的data对象值传给mapData
+        });
+        //执行实时天气信息查询
+        weather.getForecast(cityName, function (err, data) {
+          console.log(err, data);
+          _this.futureMapData = data.forecasts;
+          _this.futureMapData.map((item) => {
+            _this.seriesData.push(item.dayTemp)
+          })
+          
+          _this.initEchart();
+        });
+      });
+    },
+    initEchart() {
+      let dom = this.$refs.echartContainer;  //$refs拿到ref为echartContainer的dom结构
+      var myChart = echarts.init(dom);
+      let app = {},
+        option = null;
 
-      // AMap.plugin('AMap.Weather', function() {
-      //   //创建天气查询实例
-      //   let weather = new AMap.Weather();
-      //   let local = that.locationInfo
-      //   // console.log(local);
+      option = {
+        xAxis: {
+          type: "category",
+          data: ["今天", "明天", "后天", "三天后"],
+          axisLine: {
+            lineStyle: {
+              color: "#fff",
+            },
+          },
+          axisTick: {
+            show: false,
+          },
+        },
+        yAxis: {
+          show: false,
+          type: "value",
+        },
+        tooltip: {
+          trigger: "axis",
+          formatter: function (params) {
+            var relVal = params[0].name;
+            for (var i = 0, l = params.length; i < l; i++) {
+              relVal += params[i].value + "°C";
+            }
+            return relVal;
+          },
+        },
+        series: [
+          {
+            data: this.seriesData,
+            type: "line",
+          },
+        ],
+      };
 
-      //   //执行实时天气信息查询
-      //   weather.getForecast(local, function(err, data) {
-      //     // console.log(err, data);
-      //     // console.log(data.forecasts[0]);
-      //   });
-      // });
-    }
-  }
-}
+      myChart.setOption(option);
+    },
+  },
+};
 </script>
 
 <style lang="less">
-.container{
+.container {
   min-height: 100vh;
   background-color: #000;
   opacity: 0.7;
   color: #fff;
-  .nav{
+  .nav {
     display: flex;
     justify-content: space-between;
     padding: 10px;
   }
-  .city-info{
-    font-size: 26px;
-    em{
-      font-size: 40px;
-      font-style: normal;
+  .city-info {
+    text-align: center;
+    .temp {
+      font-size: 26px;
+      em {
+        font-size: 34px;
+        font-style: normal;
+      }
     }
+  }
+  .future {
+    margin-top: 30px;
+    padding: 0 10px;
+    .group {
+      height: 44px;
+      line-height: 44px;
+      border-radius: 4px;
+      background-color: rgba(255, 255, 255, 0.3);
+      color: rgba(16, 16, 16, 1);
+      font-size: 16px;
+      margin-bottom: 10px;
+      padding: 0 10px;
+      .tm {
+        color: #fff;
+      }
+    }
+  }
+  .echart-container {
+    width: 100%;
+    height: 50vh;
   }
 }
 </style>
