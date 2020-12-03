05bc375acc97305eb9e41526fe833b0d72dc3759
wohy <1829757133@qq.com>
2020-12-03 23:01:38 +0800

    饿了么第二次更新

--- vue/vue-elm/src/components/header/Header.vue
+++ vue/vue-elm/src/components/header/Header.vue
@@ -17,9 +17,15 @@
           <span class="text">在线支付满28减5</span>
         </div>
       </div>
+      <div class="support-count">
+        <span class="count">5个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
     <div class="bulletin-wrapper">
-
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">11111111111111111111111111111111111111111111111111111111111111111111111111111111</span>
+      <i class="icon-keyboard_arrow_right"></i>  
     </div>
   </div>
 </template>
@@ -70,9 +76,48 @@ export default {
             vertical-align middle
             font-weight bold
             line-height 1
-        .decription
+        .description
           font-size $fontsize-small
           margin-bottom 8px
           line-height 12px
-        
+        .support
+          display flex
+          align-items center
+          &-ico
+            margin-right 4px
+          .text
+            line-height 12px
+            font-size $fontsize-small-s
+      .support-count
+        position absolute
+        right 12px
+        bottom 14px
+        align-items center
+        padding 0 8px
+        height 24px
+        line-height 24px
+        text-align center
+        border-radius 14px
+        background $color-background-sss
+        .count
+          font-size $fontsize-small-s
+        .icon-keyboard_arrow_right
+          margin-left 2px
+          line-height 24px
+          font-size $fontsize-small-s
+    // .bulletin-wrapper
+    //   position relative
+    //   display flex
+    //   height 28px
+    //   line-height 28px
+    //   padding 0 8px
+    //   background $color-background-sss
+    //   .bulletin-title
+    //     flex 0 0 22px
+    //     width 22px
+    //     height 12px
+    //     margin-right 4px
+    //     bg-image('bulletin')
+    //     background-size 22px 12px
+    //     background-repeat no-repeat
 </style>
